35cb8d47b5e8bc8949addce157fc771e6262db50
Ty Miller <ty.loves.code@gmail.com>
2018-04-16 22:27:52 -0400

    initial commit

---
+++ javascript-array-methods.js
@@ -0,0 +1,20 @@
+//indexOf
+//Return the index of a specific element in the array.
+//If the element does not exist in the array, the function returns -1. 
+
+var arr = [1, 2, 100, 12, -1];
+
+arr.indexOf(100); // 2
+arr.indexOf(5); // -1
+
+//push and pop
+//Push elements to the end of the array
+//Delete the last element from an array
+
+var arr2 = [1, 2, 3, 4];
+
+arr2.push(5); // [1, 2, 3, 4, 5]
+arr2.push(6, 7, 8); // [1, 2, 3, 4, 5, 6, 7 ,8]
+
+arr2.pop(); // [1, 2, 3, 4, 5, 6, 7]
+
